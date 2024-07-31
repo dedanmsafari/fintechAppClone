6988cf09c936ff3de0444ae97aa504fa8e4154e8
dedanmsafari <dedan.msafari@gmail.com>
2024-07-31 08:19:11 +0300

    feat: font additions

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,17 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={styles.View}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.view}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  View: {
+  text: {
+    fontFamily: 'RobotoMono-Bold',
+  },
+  view: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
