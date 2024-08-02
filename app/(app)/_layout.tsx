0c1a499387f671c461d579e3ea43e5f7176fe903
dedanmsafari <dedan.msafari@gmail.com>
2024-08-02 09:57:04 +0300

    feat: signup page setup

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,9 +1,29 @@
-import { Stack } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, useRouter } from 'expo-router';
+
+import Colors from '@/constants/Colors';
 
 export default function RootLayout() {
+  const router = useRouter();
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="signUp"
+        options={{
+          headerBackTitle: '',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          title: '',
+        }}
+      />
     </Stack>
   );
 }
