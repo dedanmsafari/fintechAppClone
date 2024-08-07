16b62f22e2d2bb9970b112f755fc6e36acecafb4
dedanmsafari <dedan.msafari@gmail.com>
2024-08-07 07:01:53 +0300

    feat: tab bar pages

---
+++ app/(root)/(tabs)/lifestyle.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const lifestyle = () => {
+  return (
+    <View>
+      <Text>lifestyle</Text>
+    </View>
+  );
+};
+
+export default lifestyle;
---
+++ app/(root)/(tabs)/transfers.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const transfers = () => {
+  return (
+    <View>
+      <Text>transfers</Text>
+    </View>
+  );
+};
+
+export default transfers;
