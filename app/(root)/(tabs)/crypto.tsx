99d0d2c316eac0fa22a79321e087a522876945ef
dedanmsafari <dedan.msafari@gmail.com>
2024-08-07 07:01:53 +0300

    feat: tab bar pages

---
+++ app/(root)/(tabs)/crypto.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const crypto = () => {
+  return (
+    <View>
+      <Text>crypto</Text>
+    </View>
+  );
+};
+
+export default crypto;
