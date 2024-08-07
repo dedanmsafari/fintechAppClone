e56c0b006cd91e4efbab514a18dcd2d467fd6759
dedanmsafari <dedan.msafari@gmail.com>
2024-08-07 07:02:18 +0300

    feat: tab layout changes

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import { FontAwesome } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+import { Tabs } from 'expo-router';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import CustomHeader from '@/components/CustomHeader';
+import Colors from '@/constants/Colors';
+
+const _layout = () => {
+  const { bottom } = useSafeAreaInsets();
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarBackground: () => (
+          <BlurView
+            experimentalBlurMethod="dimezisBlurView"
+            intensity={100}
+            tint={'extraLight'}
+            style={{
+              backgroundColor: Colors.tint,
+              flex: 1,
+            }}
+          />
+        ),
+        tabBarStyle: {
+          backgroundColor: 'transparent',
+          borderTopWidth: 0,
+          bottom: 0,
+          elevation: 0,
+          left: 0,
+          paddingBottom: bottom + 10,
+          position: 'absolute',
+          right: 0,
+        },
+      }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          header: () => <CustomHeader />,
+          headerTransparent: true,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="registered" size={size} color={color} />
+          ),
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="invest"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="line-chart" size={size} color={color} />
+          ),
+          title: 'Invest',
+        }}
+      />
+      <Tabs.Screen
+        name="transfers"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="exchange" size={size} color={color} />
+          ),
+          title: 'Transfers',
+        }}
+      />
+      <Tabs.Screen
+        name="crypto"
+        options={{
+          header: () => <CustomHeader />,
+          headerTransparent: true,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="bitcoin" size={size} color={color} />
+          ),
+          title: 'Crypto',
+        }}
+      />
+      <Tabs.Screen
+        name="lifestyle"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="th" size={size} color={color} />
+          ),
+          title: 'Lifestyle',
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
