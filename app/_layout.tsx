afe8afb9ac6448f63f44f80f2f87087332db366a
dedanmsafari <dedan.msafari@gmail.com>
2024-08-08 23:57:48 +0300

    feat: clerk auth setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
+import { ClerkProvider } from '@clerk/clerk-expo';
+import Constants from 'expo-constants';
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import SessionProvider from '@/context/authContext';
+import tokenCache from '@/utils/clerkTokenCache';
+
+const publishableKey = Constants.expoConfig?.extra?.clerkPublishableKey;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
+
 export default function RootLayout() {
   return (
-    <>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Slot />
-      </GestureHandlerRootView>
-    </>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <SessionProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Slot />
+        </GestureHandlerRootView>
+      </SessionProvider>
+    </ClerkProvider>
   );
 }
