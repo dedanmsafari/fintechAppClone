2e74c5d504c5c86a4073ee2e5d7339a4b6baec92
dedanmsafari <dedan.msafari@gmail.com>
2024-08-09 22:16:05 +0300

    fix: auth context change

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -7,7 +7,7 @@ import {
   useSignIn,
   useSignUp,
 } from '@clerk/clerk-expo';
-import { router } from 'expo-router';
+import { useRouter } from 'expo-router';
 
 import { createContext } from '@/utils/context';
 import { useStorageState } from '@/utils/useStorageState';
@@ -34,45 +34,60 @@ const SessionProvider = ({ children }: PropsWithChildren) => {
   const [loading, setloading] = useState(false);
   const [code, setCode] = useState('');
 
+  const router = useRouter();
+
   const { signOut } = useAuth();
 
   const onSignUp = useSignUp();
   const onSignIn = useSignIn();
 
+  //!!When a user is Signing Up first time VerifyCode ..ok
   const verify = async () => {
+    setloading(true);
     try {
       await onSignUp.signUp!.attemptPhoneNumberVerification({
         code,
       });
       await onSignUp.setActive!({ session: onSignUp.signUp!.createdSessionId });
       setSession(onSignUp.signUp!.createdSessionId);
+      setloading(false);
     } catch (err) {
-      Alert.alert('Error', JSON.stringify(err, null, 2));
+      setloading(false);
       if (isClerkAPIResponseError(err)) {
-        Alert.alert('Error', err.errors[0].message);
+        Alert.alert(
+          'Error One',
+          err.errors[0].longMessage || err.errors[0].message
+        );
       }
     }
   };
 
-  const signInFirstTimeVerificationOTP = async () => {
+  //!!When a user is logging in back VerifyCode..ok
+  const logInVerifyCode = async () => {
+    setloading(true);
     try {
       await onSignIn.signIn!.attemptFirstFactor({
         code,
         strategy: 'phone_code',
       });
+
       await onSignUp.setActive!({ session: onSignIn.signIn!.createdSessionId });
-      setSession(onSignUp.signUp!.createdSessionId);
+
+      setSession(onSignIn.signIn!.createdSessionId);
+      setloading(false);
     } catch (err) {
-      Alert.alert('Error', JSON.stringify(err, null, 2));
+      setloading(false);
       if (isClerkAPIResponseError(err)) {
-        Alert.alert('Error', err.errors[0].message);
+        Alert.alert(
+          'Error Two',
+          err.errors[0].longMessage || err.errors[0].message
+        );
       }
     }
   };
-  const signInSecondTimeVerificationOTP = async (
-    code: string,
-    phone: string
-  ) => {
+
+  //!!When a user is logging in back..ok
+  const logIn = async (code: string, phone: string) => {
     const fullPhoneNumber = `${code}${phone}`;
 
     setloading(true);
@@ -99,38 +114,25 @@ const SessionProvider = ({ children }: PropsWithChildren) => {
       });
 
       router.push(`/verify/${fullPhoneNumber}?signin=true`);
-    } catch (err) {
-      // Default error message in case something goes wrong
-      let errorMessage = 'An unknown error occurred';
-
-      // Type-check to ensure `err` is an object
-      if (typeof err === 'object' && err !== null) {
-        const errorObj = err as {
-          status?: number;
-          clerkError?: boolean;
-          errors?: { longMessage: string }[];
-        };
-
-        // Check if `errors` exists and has at least one entry
-        if (Array.isArray(errorObj.errors) && errorObj.errors.length > 0) {
-          errorMessage = errorObj.errors[0].longMessage;
-        }
-      }
-
-      // Display the longMessage in an alert
-      Alert.alert('Error Signing In', errorMessage);
       setloading(false);
+    } catch (err) {
+      setloading(false);
+      if (isClerkAPIResponseError(err)) {
+        Alert.alert(
+          'Error Three',
+          err.errors[0].longMessage || err.errors[0].message
+        );
+      }
     }
   };
-
   const logOut = async () => {
     signOut();
     setSession(null);
   };
 
+  //!!When a user is Signing Up first time ..ok
   const signUp = async (code: string, phone: string) => {
     const fullPhoneNumber = `${code}${phone}`;
-
     setloading(true);
     if (!onSignUp.isLoaded) {
       return;
@@ -143,29 +145,15 @@ const SessionProvider = ({ children }: PropsWithChildren) => {
 
       onSignUp.signUp!.preparePhoneNumberVerification();
       router.push(`/verify/${fullPhoneNumber}`);
-
       setloading(false);
     } catch (err) {
-      // Default error message in case something goes wrong
-      let errorMessage = 'An unknown error occurred';
-
-      // Type-check to ensure `err` is an object
-      if (typeof err === 'object' && err !== null) {
-        const errorObj = err as {
-          status?: number;
-          clerkError?: boolean;
-          errors?: { longMessage: string }[];
-        };
-
-        // Check if `errors` exists and has at least one entry
-        if (Array.isArray(errorObj.errors) && errorObj.errors.length > 0) {
-          errorMessage = errorObj.errors[0].longMessage;
-        }
-      }
-
-      // Display the longMessage in an alert
-      Alert.alert('Error Signing In', errorMessage);
       setloading(false);
+      if (isClerkAPIResponseError(err)) {
+        Alert.alert(
+          'Error Four',
+          err.errors[0].longMessage || err.errors[0].message
+        );
+      }
     }
   };
 
@@ -173,10 +161,10 @@ const SessionProvider = ({ children }: PropsWithChildren) => {
     code,
     isLoading,
     loading,
-    logIn: signInSecondTimeVerificationOTP,
+    logIn,
     session,
     setCode,
-    signIn: signInFirstTimeVerificationOTP,
+    signIn: logInVerifyCode,
     signOut: logOut,
     signUp,
     verify,
