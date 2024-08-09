94d54ce64cec1ec27344f667162764e9d0e968ea
dedanmsafari <dedan.msafari@gmail.com>
2024-08-09 22:14:07 +0300

    feat: account change

--- app/(root)/(modal)/account.tsx
+++ app/(root)/(modal)/account.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
-const account = () => {
+import { router } from 'expo-router';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useSession } from '@/context/authContext';
+
+const Account = () => {
+  const { signOut } = useSession();
+  const { top } = useSafeAreaInsets();
   return (
-    <View>
-      <Text>account</Text>
+    <View style={[defaultStyles.container, { marginTop: top }]}>
+      <TouchableOpacity
+        style={[
+          defaultStyles.pillButton,
+          { backgroundColor: Colors.lightGray, marginTop: 10 },
+        ]}
+        onPress={() => signOut()}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={defaultStyles.pillButton}
+        onPress={() => router.back()}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default account;
+export default Account;
