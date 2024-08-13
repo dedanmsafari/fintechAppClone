078e659d069821d87e98705039a861b9ae2cf983
dedanmsafari <dedan.msafari@gmail.com>
2024-08-13 23:09:22 +0300

    feat: added tanstaq to the file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ClerkProvider } from '@clerk/clerk-expo';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Constants from 'expo-constants';
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -16,14 +17,18 @@ if (!publishableKey) {
   );
 }
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <SessionProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <Slot />
-        </GestureHandlerRootView>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <Slot />
+          </GestureHandlerRootView>
+        </SessionProvider>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
