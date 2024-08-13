e92b6f1e9ffb0fe8800dd6e77ec58e0906f361c5
dedanmsafari <dedan.msafari@gmail.com>
2024-08-13 23:10:12 +0300

    feat: added api routes

---
+++ app/api/info+api.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+// import Constants from 'expo-constants';
+
+// const API_KEY = Constants.expoConfig?.extra?.coinMarketCapSecretKey;
+
+export function GET(request: Request) {
+  return Response.json({ hello: 'world' });
+}
