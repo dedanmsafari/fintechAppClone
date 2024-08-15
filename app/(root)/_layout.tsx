68a2d7ca170210fecbd75903f093fc1c866b0a3f
dedanmsafari <dedan.msafari@gmail.com>
2024-08-15 16:07:16 +0300

    feat: layout change

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { TouchableOpacity, View } from 'react-native';
 
-import { Redirect, Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Redirect, router, Stack } from 'expo-router';
 
+import Colors from '@/constants/Colors';
 import { useSession } from '@/context/authContext';
 
 const AppLayout = () => {
@@ -21,6 +24,33 @@ const AppLayout = () => {
           headerTitle: '',
           headerTransparent: true,
           presentation: 'modal',
+        }}
+      />
+      <Stack.Screen
+        name="crypto/[id]"
+        options={{
+          headerLargeTitle: true,
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <View style={{ flexDirection: 'row', gap: 10 }}>
+              <TouchableOpacity>
+                <Ionicons
+                  name="notifications-outline"
+                  color={Colors.dark}
+                  size={30}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Ionicons name="star-outline" color={Colors.dark} size={30} />
+              </TouchableOpacity>
+            </View>
+          ),
+          headerTransparent: true,
+          title: '',
         }}
       />
     </Stack>
